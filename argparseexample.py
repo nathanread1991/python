2c91ee447eb537c7c711b7146aa5b9706160b576
nathanread1991 <nathanread1991@gmail.com>
2021-08-23 23:18:42 +0100

    python course on a cloud guru python for system admins course

--- argparseexample.py
+++ argparseexample.py
@@ -1,11 +1,10 @@
 import argparse
-
-import argparse
 # Build the parser
 parser = argparse.ArgumentParser(description='Reads a file in reverse', prog='file reverse')
-parser.add_argument('filename', help='the file to read in' )
+parser.add_argument('filename', help='the file to read in')
 parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s: v1.0')
+parser.add_argument('--mode', '-m', choices=['create', 'read', 'update', 'delete'])
 args = parser.parse_args()
 
 with open(args.filename) as f:
@@ -15,8 +14,8 @@ with open(args.filename) as f:
     if args.limit:
         lines = lines[:args.limit]
     for line in lines:
-      print(line.strip()[::-1])
+        print(line.strip()[::-1])
 # Parse the arguments
-
+print(args)
 
 # Read the file, reverse the contents and print
