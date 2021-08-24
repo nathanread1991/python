12129f61464ffadf7296fe1ea0c97ec13d667671
nathanread1991 <nathanread1991@gmail.com>
2021-08-24 22:42:46 +0100

    lesson on exception handling

--- argparseexample.py
+++ argparseexample.py
@@ -7,15 +7,20 @@ parser.add_argument('--version', '-v', action='version', version='%(prog)s: v1.0
 parser.add_argument('--mode', '-m', choices=['create', 'read', 'update', 'delete'])
 args = parser.parse_args()
 
-with open(args.filename) as f:
-    lines = f.readlines()
-    lines.reverse()
-
-    if args.limit:
-        lines = lines[:args.limit]
-    for line in lines:
-        print(line.strip()[::-1])
-# Parse the arguments
-print(args)
+try:
+    f = open(args.filename)
+    limit = args.limit
+except FileNotFoundError as err:
+    print(f"Error: {err}")
+else:
+    with f:
+        lines = f.readlines()
+        lines.reverse()
+        if args.limit:
+            lines = lines[:limit]
+        for line in lines:
+            print(line.strip()[::-1])
+    # Parse the arguments
+    print(args)
 
 # Read the file, reverse the contents and print
